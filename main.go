7ebb00319fd8a4a5004b043a63756c7039ba9327
eleele <2369745961@qq.com>
2019-05-24 22:34:28 +0800

    Added support for golang.org && Modified download command from wget to wget -c

--- main.go
+++ main.go
@@ -28,23 +28,27 @@ func execWithOutput(cmd *exec.Cmd, flag int) {
 	}
 	cmd.Wait()
 }
-
 func main() {
 	// Parse pkg name
 	var pkg = flag.String("p", "", "")
 	flag.Parse()
+	fmt.Println(*pkg)
 	var lst = strings.SplitN(*pkg, "/", 4)
 
 	// Chdir
 	var plst = strings.Split(os.Getenv("GOPATH"), ":")
-	os.Chdir(plst[0] + "/src/github.com")
-	os.Mkdir(lst[1], 0700)
+	os.Chdir(plst[0] + "/src/" + lst[0])
+	os.MkdirAll(lst[1], 0700)
 	os.Chdir("./" + lst[1])
 	fmt.Println(os.Getwd())
 
 	// Download
 	var url = strings.Join(lst[:3], "/")
-	var cmd = exec.Command("wget", "https://"+url+"/archive/master.zip")
+	if lst[0] == "golang.org" {
+		url = strings.Replace(url, "golang.org/x", "github.com/golang", -1)
+		fmt.Println(url)
+	}
+	var cmd = exec.Command("wget", "https://"+url+"/archive/master.zip", "-c")
 	execWithOutput(cmd, 1)
 
 	// Install
